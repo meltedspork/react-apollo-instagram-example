fc1b9d52a0d63b659b4afe43477343dd69d75b4a
Jason Salerno <jasons@jellyvision.com>
2017-09-26 13:07:23 -0500

    add new components

---
+++ src/components/Post.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+class Post extends Component {
+  render () {
+    return (
+      <div className='pa3 bg-black-05 ma3'>
+        <div
+          className='w-100'
+          style={{
+            backgroundImage: `url(${this.props.post.imageUrl})`,
+            backgroundSize: 'cover',
+            paddingBottom: '100%',
+          }}
+        />
+        <div className='pt3'>
+          {this.props.post.description}&nbsp;
+          <span className='red f6 pointer dim' onClick={this.handleDelete}>Delete</span>
+        </div>
+      </div>
+    )
+  }
+
+  handleDelete = () => {
+    // TODO: delete post before reloading posts
+    this.props.refresh()
+  }
+}
+
+export default Post
