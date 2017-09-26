0afe496c8138d90505ca45f110189e17f28f8e61
Jason Salerno <jasons@jellyvision.com>
2017-09-26 13:13:32 -0500

    add apollo client and update index files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ListPage from './components/ListPage'
+import CreatePage from './components/CreatePage'
+import { Router, Route, browserHistory } from 'react-router'
+import ApolloClient, { createNetworkInterface } from 'apollo-client'
+import { ApolloProvider } from 'react-apollo'
+import 'tachyons'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const networkInterface = createNetworkInterface({
+  uri: 'https://api.graph.cool/simple/v1/cj81mwlnz0cre0196qvftgbfz'
+})
+const client = new ApolloClient({
+  networkInterface
+})
+
+
+ReactDOM.render((
+  <ApolloProvider client={client}>
+    <Router history={browserHistory}>
+      <Route path='/' component={ListPage} />
+      <Route path='/create' component={CreatePage} />
+    </Router>
+  </ApolloProvider>
+  ),
+  document.getElementById('root')
+)
